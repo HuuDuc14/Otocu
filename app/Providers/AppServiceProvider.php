702f5b7f0ef3a09e7208aae1e486e12900d00464
HuuDuc <ducnguyenhuu829@gmail.com>
2025-02-22 18:16:57 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,14 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-        if (!User::where('email', 'admin@gmail.com')->exists()) {
-            User::create([
-                'username' => 'Admin',
-                'email' => 'admin@gmail.com',
-                'phone_number' => '0123456789',
-                'password' => Hash::make('admin'),
-                'role' => 'admin',
-            ]);
-        }
+
     }
 }
